f2dacc270a57bc557fb5b53d758f189312ebdc80
romanm777 <roman1918@gmail.com>
2016-05-27 21:26:00 +0300

    Battlefield object with a js-file appended

---
+++ js/battlefield.js
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////
+
+
+function Battlefield(width, height) {
+
+}
+
+Battlefield.prototype.getStartPosition = function (tankWidth, tankHeight) {
+
+}
+
+Battlefield.prototype.canMove = function(leftStr, topStr, dir, offset) {
+}
