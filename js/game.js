121ec6a2cf2e298db58840095b82ed64e300fee6
romanm777 <roman1918@gmail.com>
2016-05-27 21:22:00 +0300

    Game object appended

---
+++ js/game.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function () {
+  var game = new Game();
+
+
+});
+
+
+//////////////////////////////////////////////////////
+// 		Functionality
+
+// Represents battle city game
+
+function Game() {
+  $(document.body).css({
+    // "display": "flex",
+    // "justify-content": "center",
+    "background-color": "orange"
+  });
+
+}
