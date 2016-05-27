54828f677315fa93d5264e7cff1fba30f6a45006
romanm777 <roman1918@gmail.com>
2016-05-27 21:38:45 +0300

    Walls object with js file appended

--- js/game.js
+++ js/game.js
@@ -13,9 +13,14 @@ $(document).ready(function () {
 
 function Game() {
   $(document.body).css({
-    // "display": "flex",
-    // "justify-content": "center",
-    "background-color": "orange"
+      // "display": "flex",
+      // "justify-content": "center",
+      "background-color": "orange"
   });
 
+  this._battlefield = new Battlefield(700, 700);
+  var pos = this._battlefield.getStartPosition(TANK_DIMS.WIDTH, TANK_DIMS.HEIGHT);
+
+  // init position and // callback to check if move is possible
+  this._tank = new Tank(pos, this._battlefield);
 }
