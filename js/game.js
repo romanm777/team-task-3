bf7a00efcd356d2c6bc9a67f397df5d61ed2fb0e
romanm777 <roman1918@gmail.com>
2016-05-27 21:55:47 +0300

    Game object enhancing

--- js/game.js
+++ js/game.js
@@ -2,7 +2,13 @@
 $(document).ready(function () {
   var game = new Game();
 
+  document.body.onkeydown = function (e) {
+    game.onKeyDown(e);
+  }
 
+  document.body.onkeyup = function () {
+    game.onKeyUp();
+  }
 });
 
 
@@ -13,9 +19,9 @@ $(document).ready(function () {
 
 function Game() {
   $(document.body).css({
-      // "display": "flex",
-      // "justify-content": "center",
-      "background-color": "orange"
+    // "display": "flex",
+    // "justify-content": "center",
+    "background-color": "orange"
   });
 
   this._battlefield = new Battlefield(700, 700);
@@ -23,4 +29,36 @@ function Game() {
 
   // init position and // callback to check if move is possible
   this._tank = new Tank(pos, this._battlefield);
+}
+
+Game.prototype.onKeyDown = function (e) {
+  if (e.keyCode == KEYCODE_LEFT) {
+    rotate(tank, DIRECTION.LEFT);
+    move(tank, DIRECTION.LEFT);
+    tank.css("left", function(index) {
+      return (parseInt(tank.css("left")) - 10) + 'px';
+    });
+  }
+  else if (e.keyCode == KEYCODE_RIGHT) {
+    rotate(tank, DIRECTION.RIGHT);
+    move(tank, DIRECTION.RIGHT);
+  	tank.style.left = (parseInt(tank.style.left) + 10) + 'px';
+  }
+  else if (e.keyCode == KEYCODE_UP) {
+    rotate(tank, DIRECTION.UP);
+    move(tank, DIRECTION.UP);
+    tank.style.top = (parseInt(tank.style.top) - 10) + 'px';
+  }
+  else if (e.keyCode == KEYCODE_DOWN) {
+    rotate(tank, DIRECTION.DOWN);
+    move(tank, DIRECTION.DOWN);
+    tank.style.top = (parseInt(tank.style.top) + 10) + 'px';
+  }
+}
+
+Game.prototype.onKeyUp = function () {
+  //var tank = $("#tank");
+
+  // var curDir = tankObj.getDir();
+  // tank.css({'background-image' : curDir.staticPic});
 }
